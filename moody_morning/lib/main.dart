72a2acb3efd133dbed82b4abc74d476cc7f3692b
Christian <cvedelpetersen@gmail.com>
2023-06-08 22:43:09 +0200

    Created a basic scroll wheel

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
+import 'screens/set_alarm.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello There!'),
-        ),
-      ),
-    );
-  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SetAlarm(),
+  ));
 }
---
+++ moody_morning/lib/screens/set_alarm.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:moody_morning/screens/tile.dart';
+
+class SetAlarm extends StatefulWidget {
+  const SetAlarm({super.key});
+
+  @override
+  State<SetAlarm> createState() => _SetAlarmState();
+}
+
+class _SetAlarmState extends State<SetAlarm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // backgroundColor: Colors.purple.shade700,
+        // appBar: AppBar(
+        //   title: Image.asset('assets/images/logoFull.png'),
+        //   centerTitle: true,
+        //   backgroundColor: Colors.purple.shade800,
+        // ),
+        body: Center(
+      child: ListWheelScrollView.useDelegate(
+        itemExtent: 50,
+        childDelegate: ListWheelChildBuilderDelegate(
+            childCount: 10, builder: (context, index) => MyTile()),
+      ),
+    ));
+  }
+}
