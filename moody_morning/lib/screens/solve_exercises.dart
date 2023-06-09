11cd1f3b86004547fbc3ddddb9d71be06abb9c4a
sigurdjoergensen <sigurd@fajstrup.dk>
2023-06-09 16:10:33 +0200

    startede accelerometer funktion

--- moody_morning/lib/screens/solve_exercises.dart
+++ moody_morning/lib/screens/solve_exercises.dart
@@ -1,12 +1,50 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:moody_morning/widgets/logo_app_bar.dart';
+import 'package:moody_morning/system/accelerometer_functions.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+class exerciseAlarm extends StatefulWidget {
+  
 
-class solveExercises extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF423E72),
-      appBar: LogoAppBar(),
+  _solveExercises createState() => _solveExercises();
+}
+
+
+class _solveExercises extends State<exerciseAlarm> {
+ final Stream<AccelerometerEvent> _stream
+  AccelerometerEvent? _accelerometerValues;
+  @override
+  void initState(){
+    super.initState();
+    _stream.listen(AccelerometerEvent event)
+
+
+
+    }
+  
+
+
+  @override
+Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Accelerometer Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('X: '),
+              Text('Y: '),
+              Text('Z: '),
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
 }
