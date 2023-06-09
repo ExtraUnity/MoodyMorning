6a74d7ab8a4c81a8f0342e698734ed0dbd31b130
Tobias Martinsen <t.f.martinsen@gmail.com>
2023-06-09 14:16:59 +0200

    Made alarms screen

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:moody_morning/screens/alarms_screen.dart';
 import 'screens/set_alarm.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SetAlarm(),
+    home: AlarmScreen(),
   ));
 }
---
+++ moody_morning/lib/screens/alarms_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:moody_morning/system/all_alarms.dart';
+import 'package:moody_morning/widgets/logo_app_bar.dart';
+import 'package:provider/provider.dart';
+
+class AlarmScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var currentAlarms = AllAlarms();
+    currentAlarms.addAlarm(2, 24);
+    currentAlarms.addAlarm(2, 24);
+
+    return Scaffold(
+      backgroundColor: Colors.purple.shade700,
+      appBar: LogoAppBar(),
+      body: ListView(
+        children: [
+          for(var alarm in currentAlarms.alarms) 
+            Alarm(alarm: alarm),
+        ],
+      ),
+    );
+  }
+}
+
+class Alarm extends StatelessWidget {
+  const Alarm({
+    super.key,
+    required this.alarm,
+  });
+
+  final Timer alarm;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Row(children: [
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(alarm.hours.toString() + " : " + alarm.minutes.toString(), textScaleFactor: 2,),
+        ),
+        SizedBox(width: 200,),
+        OnOff(),
+      ],
+      ),
+    );
+  }
+}
+
+class OnOff extends StatefulWidget {
+  const OnOff({super.key});
+
+  @override
+  State<OnOff> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<OnOff> {
+  bool light = false;
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value: light, 
+    onChanged: (bool value) {
+       setState(() {
+          light = value;
+        });
+        print(value.toString());
+    });
+  }
+}
