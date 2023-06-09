e0ddaca2cf541b96e7a7aacb49e44670cf1cc55b
Nikolajkng <102536857+Nikolajkng@users.noreply.github.com>
2023-06-09 10:45:08 +0200

    createdAScreen

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'screens/set_alarm.dart';
+import 'package:moody_morning/screens/set_alarm.dart';
+import 'package:moody_morning/screens/solve_equation.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SetAlarm(),
+    home: solve_equation(),
   ));
 }
