db91700628649f01ca38043278005a34a4d624dd
michmai <121814049+michmai@users.noreply.github.com>
2023-06-09 11:13:11 +0200

    .

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moody_morning/screens/solve_equation.dart';
+import 'package:moody_morning/screens/set_alarm.dart';
 
 void main() {
   runApp(MaterialApp(
