0ac8258c77c240a36c015b0f3308efecdbc162da
michmai <121814049+michmai@users.noreply.github.com>
2023-06-09 12:32:32 +0200

    .

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:moody_morning/screens/solve_equation.dart';
 import 'package:moody_morning/screens/set_alarm.dart';
+import 'package:moody_morning/screens/solve_riddle.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SetAlarm(),
-    //home: solveEquation(),
+   // home: SetAlarm(),
+     home: SolveRiddle(),
+    //home: SolveEquation(),
+    //home: SolveQRcode(),
   ));
 }
