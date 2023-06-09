8931c3d16ae4634cc9bd115e1c844f9fea23055f
Nikolajkng <102536857+Nikolajkng@users.noreply.github.com>
2023-06-09 15:07:56 +0200

    Update numpad.dart

--- moody_morning/lib/widgets/numpad.dart
+++ moody_morning/lib/widgets/numpad.dart
@@ -21,24 +21,53 @@ class NumPad extends StatelessWidget {
 
 List<Widget> numButtonList() {
   List<Widget> completeButtonList = [];
+  //Creates numberButtons from 1-9 and adds to list:
   for (int i = 1; i < 10; i++) {
-    //Creates button, assign a value and add to list:
     NumButton button = NumButton(
-        onPressed: () {
-          print("$i");
-        },
-        child: Text("$i"),
-        numValue: i);
+      onPressed: () {
+        print("$i");
+      },
+      child: Text("$i"),
+      numValue: i,
+    );
     completeButtonList.add(button);
   }
+
+  //Creates the last 3 "functional" buttons (back- & enter button and 0):
+  NumButton button0 = NumButton(
+      onPressed: () {
+        print("0");
+      },
+      child: Text("0"),
+      numValue: 0);
+
+  NumButton buttonBack = NumButton(
+      onPressed: () {
+        print("pressed Back...");
+      },
+      child: Text("Back"),
+      numValue: 10);
+
+  NumButton buttonEnter = NumButton(
+      onPressed: () {
+        print("pressed Enter...");
+      },
+      child: Text("Enter"),
+      numValue: 12);
+
+  completeButtonList.addAll([buttonBack, button0, buttonEnter]);
+
   return completeButtonList;
 }
 
 class NumButton extends ElevatedButton {
   final int numValue;
-  const NumButton(
-      {super.key,
-      required super.onPressed,
-      required super.child,
-      required this.numValue}); //Separate button-instances by value
+
+  const NumButton({
+    super.key,
+    required super.onPressed,
+    required super.child,
+    required this.numValue,
+    ButtonStyle? super.style,
+  });
 }
