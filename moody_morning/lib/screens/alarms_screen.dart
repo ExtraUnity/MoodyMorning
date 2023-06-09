02fb3dd9786abdef29a877e9e510f49be22f9900
Tobias Martinsen <t.f.martinsen@gmail.com>
2023-06-09 14:57:37 +0200

    differentiate between switches

--- moody_morning/lib/screens/alarms_screen.dart
+++ moody_morning/lib/screens/alarms_screen.dart
@@ -15,8 +15,8 @@ class AlarmScreen extends StatelessWidget {
       appBar: LogoAppBar(),
       body: ListView(
         children: [
-          for(var alarm in currentAlarms.alarms) 
-            Alarm(alarm: alarm),
+          for(int i = 0; i < currentAlarms.alarms.length; i++) 
+            Alarm(alarm: currentAlarms.alarms[i], numb: i),
         ],
       ),
     );
@@ -27,9 +27,11 @@ class Alarm extends StatelessWidget {
   const Alarm({
     super.key,
     required this.alarm,
+    required this.numb,
   });
 
   final Timer alarm;
+  final int numb;
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +43,7 @@ class Alarm extends StatelessWidget {
           child: Text(alarm.hours.toString() + " : " + alarm.minutes.toString(), textScaleFactor: 2,),
         ),
         SizedBox(width: 200,),
-        OnOff(),
+        OnOff(numb: numb),
       ],
       ),
     );
@@ -49,8 +51,8 @@ class Alarm extends StatelessWidget {
 }
 
 class OnOff extends StatefulWidget {
-  const OnOff({super.key});
-
+  const OnOff({super.key, required this.numb});
+  final int numb;
   @override
   State<OnOff> createState() => _MyWidgetState();
 }
@@ -64,7 +66,7 @@ class _MyWidgetState extends State<OnOff> {
        setState(() {
           light = value;
         });
-        print(value.toString());
+        print(widget.numb);
     });
   }
 }
