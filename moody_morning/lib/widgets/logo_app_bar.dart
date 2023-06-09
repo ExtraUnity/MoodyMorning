fe1d00b592319a01d0c6dbbcf12b30cb3fca1d80
Christian <cvedelpetersen@gmail.com>
2023-06-09 10:27:01 +0200

    Formatting

---
+++ moody_morning/lib/widgets/logo_app_bar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LogoAppBar extends AppBar {
+  LogoAppBar({super.key})
+      : super(
+            title: Image.asset('assets/images/logoFull.png'),
+            centerTitle: true,
+            backgroundColor: Colors.purple.shade800);
+}
