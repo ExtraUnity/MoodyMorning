35cf50ea097b285f0b3ebb1f7139f59eee648370
Tobias Martinsen <t.f.martinsen@gmail.com>
2023-06-12 15:04:44 +0200

    nav

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -6,6 +6,11 @@ import 'package:moody_morning/screens/set_alarm.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: AlarmScreen(),
+    //home: AlarmScreen(),
+    routes: {
+      '/': (context) => AlarmScreen(),
+      '/setAlarm': (context) => SetAlarm(),
+
+    }
   ));
 }
