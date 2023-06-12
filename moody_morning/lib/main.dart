4fa01d668eb21451fc21c1d18aa2d0bdafa4ce84
Christian <cvedelpetersen@gmail.com>
2023-06-13 00:35:52 +0200

    Changed the screen

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moody_morning/screens/solve_equation.dart';
 import 'package:moody_morning/screens/set_alarm.dart';
 import 'package:moody_morning/screens/solve_QRcode.dart';
+import 'package:moody_morning/screens/solve_exercises.dart';
 import 'package:moody_morning/system/all_alarms.dart';
 import 'package:moody_morning/screens/alarms_screen.dart';
 import 'package:alarm/alarm.dart';
