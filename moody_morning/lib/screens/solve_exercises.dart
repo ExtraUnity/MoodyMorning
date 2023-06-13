786915093e6257661bac770e4482f6cbed6e985d
sigurdjoergensen <sigurd@fajstrup.dk>
2023-06-13 14:01:24 +0200

    squats functioning

--- moody_morning/lib/screens/solve_exercises.dart
+++ moody_morning/lib/screens/solve_exercises.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:moody_morning/system/accelerometer_functions.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:math';
+import 'package:moody_morning/system/defines_exercise.dart';
 
 
   
@@ -14,11 +17,20 @@ class SolveExercises extends StatefulWidget {
 
 class _SolveExercisesState extends State<SolveExercises> {
   UserAccelerometerEvent? eve;
+  Accelerometer accel = Accelerometer();
+  int exercisesDone = 0;
+  bool isUp = false;
+  bool isDown = false;
+  String instruction = 'Not supposed to be seen!';
   @override
   void initState(){
     userAccelerometerEvents.listen((UserAccelerometerEvent event) {
       setState(() {
         eve = event;
+        accel.x = event.x;
+        accel.y = event.y;
+        accel.z = event.z;
+        exerciseHandler();
       });
      });
     super.initState();
@@ -35,13 +47,60 @@ class _SolveExercisesState extends State<SolveExercises> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Text("$instruction"),
+              Text("Amount of ${CURRENT_EXERCISE}: $exercisesDone'/'$AMOUNT_EXERCISE " ),
               Text('X: ${eve?.x.toStringAsFixed(3)} '),
               Text('Y: ${eve?.y.toStringAsFixed(3)}'),
               Text('Z: ${eve?.z.toStringAsFixed(3)}'),
+              Text('magnitude: ${accel.toMagnitude(accel.x,accel.y,accel.z)}')
             ],
           ),
         ),
       ),
     );
+  }
+  
+  void exerciseHandler() {
+    if(exercisesDone > AMOUNT_EXERCISE){
+      instruction = 'Finished!';
+      //afslut alarm
+    }
+    
+    else if(exercisesDone%2==0){
+      instruction = 'Down!';
+    }
+    else {
+      instruction = 'Up!';
+    }
+    if(exercisesDone%2==0){ //for Down!
+      if(!isDown && accel.y < ACCELERATION_SIZE_NEG){ //down movement = -y acceleration
+        isDown = true;
+      }
+      else if(isDown && !isUp && (accel.y > ACCELERATION_SIZE)) { //up movement = +y acceleration
+        isUp = true;
+      }
+      else if(isDown && isUp && (accel.y.abs()<ACCELERATION_SIZE)){
+        isDown = false;
+        isUp = false;
+        exercisesDone++;
+      } //vi har set at bruger er accelereret ned,så decelereret, og nu stop telefon
+    }
+    else{ //for Up!
+      if(!isUp && accel.y > ACCELERATION_SIZE){ //up movement = +y acceleration
+        isUp = true;
+      }
+      else if(isUp && !isDown && (accel.y < ACCELERATION_SIZE_NEG)) { //down movement = -y acceleration
+        isDown = true;
+      }
+      else if(isDown && isUp && (accel.y.abs()<ACCELERATION_SIZE)){
+        isDown = false;
+        isUp = false;
+        exercisesDone++;
+      } //vi har set at bruger er accelereret ned,så decelereret, og nu stop telefon
+    }
+    
+
+
+
   }
 }
