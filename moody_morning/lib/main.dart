c2211b214dc8e62e6ab9a4494a22ce3f4572bd83
ChristofferFrost <114985641+ChristofferFrost@users.noreply.github.com>
2023-06-13 12:10:33 +0200

    get

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -5,12 +5,13 @@ import 'package:moody_morning/screens/set_alarm.dart';
 import 'package:moody_morning/screens/solve_QRcode.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     //home: SetAlarm(),
     //home: solveEquation(),
-    home: MainScreen(),
+    home: MyHomePageState(),
 
   ));
 }
--- moody_morning/lib/screens/solve_QRcode.dart
+++ moody_morning/lib/screens/solve_QRcode.dart
@@ -1,68 +1,143 @@
-//import 'dart:math';
+import 'dart:math';
+import 'dart:typed_data';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 //import 'package:moody_morning/widgets/logo_app_bar.dart';
+import 'dart:collection';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
+class MyHomePageState extends StatelessWidget {
+  MobileScannerController cameraController = MobileScannerController();
+  bool _screenOpened = false;
 
-
-
-class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-
-class _MainScreenState extends State<MainScreen> {
-  late List<CameraDescription> cameras;
-  late CameraController cameraController;
-
-  @override
-  void initState(){
-    startCamera();
-    super.initState();
-  }
-
-  void startCamera() async{
-    cameras = await availableCameras();
-
-    cameraController = CameraController(
-      cameras[0], 
-      ResolutionPreset.high,
-            enableAudio: false
-      );
-
-    await cameraController.initialize().then((value){
-      if (!mounted){
-        return;
-      }
-      setState(() {}); //To refresh widget
-    }).catchError((e){
-      print(e);
-  
-    });
-  
-  }
-  @override
-  void dispose(){
-    cameraController.dispose();
-    super.dispose();
-  }
-
+  // Flashlight
   @override
   Widget build(BuildContext context) {
-    if(cameraController.value.isInitialized){
-      return Scaffold(
-          body: Stack(
-            children: [
-              CameraPreview(cameraController),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobile Scanner'),
+        actions: [
+          /*
+          IconButton(
+            color: Colors.blue,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
+          ),*/
+          IconButton(
+            //front or back camera
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.switchCamera(),
           ),
-      );
-    } else {
-      return const SizedBox();
+        ],
+      ),
+      body: MobileScanner(
+        // fit: BoxFit.contain,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          //final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+            _foundBarcode(barcode, context);
+          }
+        },
+      ),
+    );
+  }
 
+  void _foundBarcode(Barcode barcode, BuildContext context) {
+    ///open screen
+    if (!_screenOpened) {
+      final String code = barcode.rawValue ?? "---";
+      debugPrint('Barcode found! $code');
+      _screenOpened = true;
+      Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) =>
+                FoundCodeScreen(screemClosed: _screenWasClosed, value: code),
+          ));
+    }
+  }
+
+  void _screenWasClosed() {
+    _screenOpened = false;
   }
 }
+
+class FoundCodeScreen extends StatefulWidget {
+  final String value;
+  final Function() screemClosed;
+  const FoundCodeScreen({
+    Key? key,
+    required this.value,
+    required this.screemClosed,
+  }) : super(key: key);
+
+  @override
+  State<FoundCodeScreen> createState() => _FoundCodeScreen();
+}
+
+class _FoundCodeScreen extends State<FoundCodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Found Code'),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            //widget._screenClosed();
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_outlined,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "scanned Code:",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              widget.value,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
