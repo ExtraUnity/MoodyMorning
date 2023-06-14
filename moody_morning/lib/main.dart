5359f4b98a1b5b4a10082cd92bd979410ed73fee
Nikolajkng <102536857+Nikolajkng@users.noreply.github.com>
2023-06-14 19:16:48 +0200

    fixed_merge

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moody_morning/screens/solve_equation.dart';
 import 'package:moody_morning/screens/set_alarm.dart';
-import 'package:moody_morning/screens/solve_QRcode.dart';
-import 'package:moody_morning/screens/solve_exercises.dart';
+import 'package:moody_morning/screens/solve_equation.dart';
 import 'package:moody_morning/system/all_alarms.dart';
 import 'package:moody_morning/screens/alarms_screen.dart';
 import 'package:alarm/alarm.dart';
@@ -17,8 +15,7 @@ void main() async {
         routes: {
           '/': (context) => AlarmScreen(),
           '/setAlarm': (context) => SetAlarm(),
-          '/equationSettings': (context) =>
-              AlarmScreen(), //TODO: Change to equation settings
+          '/equationSettings': (context) => SolveEquation(),
           '/exerciseSettings': (context) =>
               AlarmScreen(), //TODO: Change to exercise settings
           '/QRSettings': (context) => AlarmScreen(), //TODO: Change to QR settings
