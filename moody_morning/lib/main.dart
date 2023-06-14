6971874caae2087b885df05788630baf2ac74350
Christian <cvedelpetersen@gmail.com>
2023-06-14 15:49:34 +0200

    added import to resolve error

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:moody_morning/screens/set_alarm.dart';
 import 'package:moody_morning/screens/solve_QRcode.dart';
 import 'package:moody_morning/screens/alarms_screen.dart';
 import 'package:alarm/alarm.dart';
+import 'package:moody_morning/system/all_alarms.dart';
 import 'package:provider/provider.dart';
 
 import 'package:moody_morning/system/notification_service.dart';
@@ -50,24 +51,26 @@ Future<void> main() async {
   if (notificationAppLaunchDetails?.didNotificationLaunchApp ?? false) {
     initialRoute = 'QRChallenge';
   }
-  runApp(ChangeNotifierProvider(
-    create: (context) => AllAlarms(),
-    child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: initialRoute,
-        //home: AlarmScreen(),
-        routes: {
-          '/': (context) => AlarmScreen(),
-          '/setAlarm': (context) => SetAlarm(),
-          '/equationSettings': (context) =>
-              AlarmScreen(), //TODO: Change to equation settings
-          '/exerciseSettings': (context) =>
-              AlarmScreen(), //TODO: Change to exercise settings
-          '/QRSettings': (context) => AlarmScreen(), //TODO: Change to QR settings
-          '/gameSettings': (context) =>
-              AlarmScreen(), //TODO: Change to game settings
-       }),
-     ),
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AllAlarms(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: initialRoute,
+          //home: AlarmScreen(),
+          routes: {
+            '/': (context) => AlarmScreen(),
+            '/setAlarm': (context) => SetAlarm(),
+            '/equationSettings': (context) =>
+                AlarmScreen(), //TODO: Change to equation settings
+            '/exerciseSettings': (context) =>
+                AlarmScreen(), //TODO: Change to exercise settings
+            '/QRSettings': (context) =>
+                AlarmScreen(), //TODO: Change to QR settings
+            '/gameSettings': (context) =>
+                AlarmScreen(), //TODO: Change to game settings
+          }),
+    ),
   );
   await Alarm.init();
 }
