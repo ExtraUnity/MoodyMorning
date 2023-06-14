e1aac8ab2361a540fdefedf541bbe2153da5c68a
Christian <cvedelpetersen@gmail.com>
2023-06-14 20:10:14 +0200

    added function to find active AlarmData

--- moody_morning/lib/system/alarm_callback.dart
+++ moody_morning/lib/system/alarm_callback.dart
@@ -1,7 +1,20 @@
 import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
 import 'package:moody_morning/screens/alarms_screen.dart';
+import 'package:moody_morning/system/all_alarms.dart';
+import 'package:provider/provider.dart';
 
 void handleAlarm(BuildContext context, AlarmSettings activeAlarm) async {
+  var allAlarms = context.watch<AllAlarms>();
+  AlarmData alarmData = getAlarmData(activeAlarm.id, allAlarms.alarms)!;
   await showNotification();
+}
+
+AlarmData? getAlarmData(int alarmID, List<AlarmData> allAlarms) {
+  for (AlarmData alarm in allAlarms) {
+    if (alarm.alarmsetting.id == alarmID) {
+      return alarm;
+    }
+  }
+  return null;
 }
