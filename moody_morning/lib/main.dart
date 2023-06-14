0906b317e1858a00b61140b8b93af4a2322da989
ChristofferFrost <114985641+ChristofferFrost@users.noreply.github.com>
2023-06-14 16:21:48 +0200

    newScanner

--- moody_morning/lib/main.dart
+++ moody_morning/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:moody_morning/screens/alarms_screen.dart';
 import 'package:moody_morning/screens/solve_equation.dart';
 import 'package:moody_morning/screens/set_alarm.dart';
 import 'package:moody_morning/screens/solve_QRcode.dart';
+import 'package:moody_morning/screens/solve_v1.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 
@@ -11,7 +12,7 @@ void main() {
     debugShowCheckedModeBanner: false,
     //home: SetAlarm(),
     //home: solveEquation(),
-    home: MyHomePageState(),
+    home: MyHomePageState1(),
 
   ));
 }
