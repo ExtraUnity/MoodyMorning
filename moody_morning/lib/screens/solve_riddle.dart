921ae5b009900f27782145d7776ff13e5bc3098e
Christian <cvedelpetersen@gmail.com>
2023-06-15 22:04:34 +0200

    ensured that all puzzled will be solvable

--- moody_morning/lib/screens/solve_riddle.dart
+++ moody_morning/lib/screens/solve_riddle.dart
@@ -10,8 +10,8 @@ class SolveRiddle extends StatefulWidget {
 }
 
 class _SlidePuzzleBoardState extends State<SolveRiddle> {
-
-  List<int> tiles = List.generate(9, (index) => index); // Create a list of tile numbers
+  List<int> tiles =
+      List.generate(9, (index) => index); // Create a list of tile numbers
 
   @override
   void initState() {
@@ -20,7 +20,21 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
   }
 
   void shuffleBoard() {
-    tiles.shuffle();
+    //make sure that
+    while (!isSolvable()) {
+      tiles.shuffle();
+    }
+    print(tiles.toString());
+  }
+
+  //board is solvable only if there are an even amount of 'switches' that need to be done
+  bool isSolvable() {
+    int switches = 0;
+    var filledTiles = tiles.where((tileValue) => tileValue != 8);
+    for (int i = 0; i < filledTiles.length - 1; i++) {
+      if (filledTiles.elementAt(i) > filledTiles.elementAt(i + 1)) switches++;
+    }
+    return switches % 2 == 0;
   }
 
   bool alarmOff() {
@@ -35,7 +49,7 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: ()async => false,
+      onWillPop: () async => false,
       child: Scaffold(
         backgroundColor: Color(0xFF423E72),
         appBar: LogoAppBar(),
@@ -45,11 +59,10 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
             Padding(
               padding: EdgeInsets.all(40.0),
               child: ElevatedButton(
-                onPressed: () {
-                  
-                },
+                onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF8F8BBF), // Change the button color here
+                  backgroundColor:
+                      Color(0xFF8F8BBF), // Change the button color here
                 ),
                 child: const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -58,10 +71,13 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
                       Icons.access_alarm,
                       size: 48,
                     ), // Add the desired icon here
-                    SizedBox(width: 15.0), // Add some spacing between the icon and the text
+                    SizedBox(
+                        width:
+                            15.0), // Add some spacing between the icon and the text
                     Text(
                       '7:30',
-                      style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
@@ -140,7 +156,7 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
                           tiles[index] = 8;
                           if (alarmOff()) {
                             // If the board is in the right order, show a dialog indicating the win
-                           challengeSolved(context);
+                            challengeSolved(context);
                           }
                         });
                       }
@@ -149,11 +165,15 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
                       alignment: Alignment.center,
                       padding: const EdgeInsets.all(49),
                       margin: EdgeInsets.all(2),
-                      color: tiles[index] != 8 ? Color(0xFF8F8BBF) : Colors.white, // Set tile color
+                      color: tiles[index] != 8
+                          ? Color(0xFF8F8BBF)
+                          : Colors.white, // Set tile color
                       child: Center(
                         child: Container(
                           child: Text(
-                            tiles[index] != 8 ? tiles[index].toString() : '', // Display tile number
+                            tiles[index] != 8
+                                ? tiles[index].toString()
+                                : '', // Display tile number
                             style: const TextStyle(
                               fontSize: 24,
                               color: Colors.white,
@@ -192,11 +212,10 @@ class _SlidePuzzleBoardState extends State<SolveRiddle> {
   }
 
   void checkSlide(int index, BuildContext context) {
-     tiles[index - 1] = tiles[index];
+    tiles[index - 1] = tiles[index];
     tiles[index] = 8;
     if (alarmOff()) {
       challengeSolved(context);
-
     }
   }
 }
