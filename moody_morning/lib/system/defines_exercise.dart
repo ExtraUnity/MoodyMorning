a8178d22df875dc7bedb85ef53f9718965f16fe0
sigurdjoergensen <sigurd@fajstrup.dk>
2023-06-15 15:00:34 +0200

    graphical changes

--- moody_morning/lib/system/defines_exercise.dart
+++ moody_morning/lib/system/defines_exercise.dart
@@ -1,5 +1,5 @@
-const int AMOUNT_EXERCISE = 10; //mængder af exercise man skal lave, ex 10 squats
-const double ACCELERATION_SIZE = 1.5; //størrelse af acceleration for aktivere threshold (m/s^2)
-const double ACCELERATION_SIZE_NEG = -1.5;
-const bool USE_XYZ_VECTOR = true; //true = sqrt(x*x+y*y+z*z), false = y(op ned)
+const int AMOUNT_EXERCISE = 20; //mængder af exercise man skal lave, ex 10 squats
+const double ACCELERATION_SIZE = 1.0; //størrelse af acceleration for aktivere threshold (m/s^2)
+const double ACCELERATION_SIZE_NEG = -1.0;
+const bool USE_XYZ_VECTOR = false; //true = sqrt(x*x+y*y+z*z), false = y(op ned)
 const String CURRENT_EXERCISE = 'squats';
