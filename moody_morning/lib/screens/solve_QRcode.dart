858b017a320d3bde78cced0888ca7cb873ebf1e8
Christian <cvedelpetersen@gmail.com>
2023-06-15 11:28:00 +0200

    claned qr files

--- moody_morning/lib/screens/solve_QRcode.dart
+++ moody_morning/lib/screens/solve_QRcode.dart
@@ -1,14 +1,10 @@
-import 'dart:math';
-import 'dart:typed_data';
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-//import 'package:moody_morning/widgets/logo_app_bar.dart';
-import 'dart:collection';
+
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'dart:ui'; // Import for ImageFilter.blur
 
+const bgColor = Color(0xFF423E72);
 
-class MyHomePageState extends StatelessWidget {
+class SolveQRCode extends StatelessWidget {
   MobileScannerController cameraController = MobileScannerController();
   bool _screenOpened = false;
 
@@ -16,84 +12,113 @@ class MyHomePageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mobile Scanner'),
-        actions: [
-          /*
-          IconButton(
-            color: Colors.blue,
-            icon: ValueListenableBuilder(
-              valueListenable: cameraController.torchState,
-              builder: (context, state, child) {
-                switch (state as TorchState) {
-                  case TorchState.off:
-                    return const Icon(Icons.flash_off, color: Colors.grey);
-                  case TorchState.on:
-                    return const Icon(Icons.flash_on, color: Colors.yellow);
-                }
-              },
+        backgroundColor: bgColor,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "QR Scanner",
+            style: TextStyle(
+              color: Colors.black87,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
             ),
-            iconSize: 32.0,
-            onPressed: () => cameraController.toggleTorch(),
-          ),*/
-          IconButton(
-            //front or back camera
-            color: Colors.white,
-            icon: ValueListenableBuilder(
-              valueListenable: cameraController.cameraFacingState,
-              builder: (context, state, child) {
-                switch (state as CameraFacing) {
-                  case CameraFacing.front:
-                    return const Icon(Icons.camera_front);
-                  case CameraFacing.back:
-                    return const Icon(Icons.camera_rear);
-                }
-              },
+          ),
+          actions: [
+            IconButton(
+              //front or back camera
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.cameraFacingState,
+                builder: (context, state, child) {
+                  switch (state as CameraFacing) {
+                    case CameraFacing.front:
+                      return const Icon(Icons.camera_front);
+                    case CameraFacing.back:
+                      return const Icon(Icons.camera_rear);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.switchCamera(),
             ),
-            iconSize: 32.0,
-            onPressed: () => cameraController.switchCamera(),
-          ),
-        ],
-      ),
-      body: Center(
-  child: Stack(
-    alignment: Alignment.center,
-    children: <Widget>[
-      Container(
-        width: MediaQuery.of(context).size.width * 0.6,  
-        height: MediaQuery.of(context).size.height * 0.3,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
+          ],
         ),
-        child: MobileScanner(
-          // fit: BoxFit.contain,
-          onDetect: (capture) {
-            final List<Barcode> barcodes = capture.barcodes;
-            for (final barcode in barcodes) {
-              debugPrint('Barcode found! ${barcode.rawValue}');
-              _foundBarcode(barcode, context);
-          }
-        },
-      ),
-    ),
-    Container(
-        width: MediaQuery.of(context).size.width * 0.6,
-        height: MediaQuery.of(context).size.height * 0.3,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.green, width: 3),
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent, width: 15),
-            color: Colors.black.withOpacity(0.3),
-          ),
-        ),
-      ),
-    ],
-  ),
-)
-    );
+        body: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          child: Column(children: [
+            const Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Place the QR code in the area',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Scanning will be started automatically',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white60,
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+              flex: 4,
+              child: Center(
+                  child: Container(
+                height: 320,
+                width: 320,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.blue, width: 10),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: SizedBox(
+                    height: 300,
+                    width: 300,
+                    child: Stack(
+                      children: [
+                        MobileScanner(
+                          // fit: BoxFit.contain,
+                          onDetect: (capture) {
+                            final List<Barcode> barcodes = capture.barcodes;
+                            for (final barcode in barcodes) {
+                              debugPrint('Barcode found! ${barcode.rawValue}');
+                              _foundBarcode(barcode, context);
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+            ),
+            Expanded(
+                child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                'MoodyMorning',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 14,
+                  letterSpacing: 1,
+                ),
+              ),
+            )),
+          ]),
+        ));
   }
 
   void _foundBarcode(Barcode barcode, BuildContext context) {
@@ -102,11 +127,10 @@ class MyHomePageState extends StatelessWidget {
       final String code = barcode.rawValue ?? "---";
       debugPrint('Barcode found! $code');
       _screenOpened = true;
-      Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) =>
-                FoundCodeScreen(screemClosed: _screenWasClosed, value: code),
-          ));
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) =>
+            FoundCodeScreen(screemClosed: _screenWasClosed, value: code),
+      ));
     }
   }
 
@@ -170,4 +194,3 @@ class _FoundCodeScreen extends State<FoundCodeScreen> {
     );
   }
 }
-
