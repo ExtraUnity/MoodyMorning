605f1b203add44b1c18776d24037318b74307955
Nikolajkng <102536857+Nikolajkng@users.noreply.github.com>
2023-06-15 16:11:09 +0200

    added WillPopScope

--- moody_morning/lib/screens/solve_equation.dart
+++ moody_morning/lib/screens/solve_equation.dart
@@ -8,10 +8,13 @@ import '../system/numpad_reader.dart';
 class SolveEquation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF423E72),
-      appBar: LogoAppBar(), //AppBar
-      body: EquationScreenLayout(),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        backgroundColor: Color(0xFF423E72),
+        appBar: LogoAppBar(), //AppBar
+        body: EquationScreenLayout(),
+      ),
     );
   }
 }
