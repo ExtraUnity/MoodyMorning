923896196474f2377737a346609f23a127533c98
Christian <cvedelpetersen@gmail.com>
2023-06-16 10:15:22 +0200

    Removed back button form app bar

--- moody_morning/lib/widgets/logo_app_bar.dart
+++ moody_morning/lib/widgets/logo_app_bar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class LogoAppBar extends AppBar {
   LogoAppBar({super.key})
       : super(
+            automaticallyImplyLeading: false,
             title: Image.asset('assets/images/logoFull.png'),
             centerTitle: true,
             backgroundColor: const Color(0xFF423E72));
